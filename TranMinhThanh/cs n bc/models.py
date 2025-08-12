3e1e765c50555d8bfd56717ed04f494838306fb6
thanh11223 <127570744+thanh11223@users.noreply.github.com>
2025-08-12 08:25:26 +0700

    Delete TranMinhThanh directory

--- TranMinhThanh/cs n bc/models.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Student(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    student_id = db.Column(db.String(10), unique=True, nullable=False)
-    name = db.Column(db.String(100), nullable=False)
-    class_name = db.Column(db.String(50), nullable=False)
